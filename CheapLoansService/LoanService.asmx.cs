7b50fa3a3cb6f59fbfcd371a23f270adb1603c0c
Colby-Snow <57962827+Colby-Snow@users.noreply.github.com>
2020-10-20 12:57:18 -0500

    Services

---
+++ CheapLoansService/LoanService.asmx.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CheapLoansService
+{
+    /// <summary>
+    /// Summary description for LoanService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class LoanService : System.Web.Services.WebService
+    {
+
+    }
+}
